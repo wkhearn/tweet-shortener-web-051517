b7f47f94cf24193806541522e97302a470f357a9
Kenny Hearn <wkhearn@gmail.com>
2017-05-05 19:29:25 +0000

    Done.

--- spec/tweet_shortener_spec.rb
+++ spec/tweet_shortener_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe 'tweet shortener' do
 
   let(:tweet_one)   {"Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"}
-  let(:tweet_two)   {"OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?"} 
+  let(:tweet_two)   {"OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?"}
   let(:tweet_three) {"GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"}
-  let(:tweet_four)  {"New game. Middle aged tweet followed by #youngPeopleHashTag Example: Gotta get my colonoscopy and mammogram soon. Prevention is key! #swag"} 
+  let(:tweet_four)  {"New game. Middle aged tweet followed by #youngPeopleHashTag Example: Gotta get my colonoscopy and mammogram soon. Prevention is key! #swag"}
   let(:tweet_five)  {"I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real."}
 
   let(:tweet_one_short)   {"Hey guys, can anyone teach me how 2 b cool? I really want 2 b the best @ everything, u know what I mean? Tweeting is super fun u guys!!!!"}
-  let(:tweet_two_short)   {"OMG u guys, u won't believe how sweet my kitten is. My kitten is like super cuddly & 2 cute 2 b believed right?"} 
+  let(:tweet_two_short)   {"OMG u guys, u won't believe how sweet my kitten is. My kitten is like super cuddly & 2 cute 2 b believed right?"}
   let(:tweet_three_short) {"GUISEEEEE this is so fun! I'm tweeting 4 u guys & this tweet is SOOOO long it's gonna b way more than u would think twitter can handle, so shorten it up u know what I mean? I just can never tell how long 2 keep typing!"}
   let(:tweet_four_short)  {"New game. Middle aged tweet followed by #youngPeopleHashTag Example: Gotta get my colonoscopy & mammogram soon. Prevention is key! #swag"}
   let(:tweet_five_short)  {"I'm running out of example tweets 4 u guys, which is weird, because I'm a writer & this is just writing & I tweet all day. 4 real, u guys. 4 real."}
@@ -24,7 +24,7 @@ describe 'tweet shortener' do
   end
 
   # Question 2
-  describe '#bulk_tweet_shortener' do  
+  describe '#bulk_tweet_shortener' do
     it 'shortens each tweet and prints the results' do
       expect($stdout).to receive(:puts).with(tweet_one_short)
       expect($stdout).to receive(:puts).with(tweet_two_short)
@@ -54,13 +54,13 @@ describe 'tweet shortener' do
   end
 
   # Question 4
-  describe '#shortened_tweet_truncator' do    
+  describe '#shortened_tweet_truncator' do
     it 'truncates tweets over 140 characters after shortening' do
       expect(shortened_tweet_truncator(tweet_three).length).to be == 140
     end
 
     it 'does not shorten tweets shorter than 140 characters.' do
-      expect(shortened_tweet_truncator(tweet_one_short).length).to be == tweet_one_short.length 
+      expect(shortened_tweet_truncator(tweet_one_short).length).to be == tweet_one_short.length
     end
   end
 
--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,47 @@
 # Write your code here.
+
+require 'pry'
+
+def dictionary
+  dictionary = {"hello" => "hi","to"=>"2","two"=>"2","too"=>"2",
+    "for"=>"4", "For"=>"4","four"=>"4","be"=>"b","you"=>"u",
+    "at"=>"@","and"=>"&"}
+end
+
+def word_substituter(tweet)
+#  dictionary_compare = dictionary.key
+#  replacement_tweet = tweet.split
+  (tweet.split.map do |index|
+    if dictionary.keys.include?(index)
+      index = dictionary[index]
+    elsif !dictionary.keys.include?(index)
+      index = index
+    end
+  end).join(" ")
+#  Method found on StackOverflow below:
+#  tweet.gsub(/#{dictionary.keys.join("|")}/i) {|index| index = dictionary[index]}
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.map do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+#  tweets.map do |tweet|
+    if tweet.length > 141
+      word_substituter(tweet)
+    else tweet
+    end
+#  end
+end
+
+def shortened_tweet_truncator(tweet)
+  short_tweet = selective_tweet_shortener(tweet)
+  if short_tweet.length > 140
+    new_short_tweet = short_tweet[0..136]<<"..."
+    new_short_tweet
+  else tweet
+  end
+end
